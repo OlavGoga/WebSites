aebf2e4064ae4b32537b1277636650e815b2045d
OlavGoga <131377307+OlavGoga@users.noreply.github.com>
2023-04-25 22:20:11 +0600

    Add files via upload

---
+++ 12.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<title>MILAPO</title>
+<link rel="stylesheet" href="style.css">
+<meta charset="UTF-8">
+</head>
+<body>
+<div class="menu">
+<ul>
+<li><a href="#" style="text-decoration:none">ABOUT OUR BRIGADE</li></a>
+<li><a href="#" style="text-decoration:none">ABOUT OUR TEAM</li></a>
+<li><a href="#" style="text-decoration:none">ABOUT OUR COMPANY</li></a>
+<li><a href="#" style="text-decoration:none">ABOUT STORY OF OUR COMPANY</li></a>
+<li><a href="#" style="text-decoration:none">CONTACTS</li></a>
+<li><a href="#" style="text-decoration:none">TYPES OF INSURANCES</li></a>
+</ul>
+</div>
+<h1>WELCOME BACK</h1>
+<div class="login-box">
+	<div class="login-box">
+	<div class="textbox">
+		<i class="fa fa-user" aria-hidden="true"></i>
+	<input type="text" placeholder="YOUR NICKNAME" name="" value="">
+</div>
+	<div class="textbox">
+	<i class="fa fa-lock" aria-hidden="true"></i>
+	<input type="password" placeholder="YOUR ID" password="" value="">
+</div>
+		<input class="btn" type ="button" value="Come back">
+	
+</div>
+
+</body>
+</html>
